d8fcbfe7db2f2bc6f635ee14c90d0fa0c87c5480
subh <harvirbuttar2917@gmail.com>
2024-09-19 18:43:48 -0700

    [Sass | setup] integrated and configured scss in app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** Byimaan */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-export default nextConfig;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
